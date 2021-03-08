04838661c2c3a1ac7adeb077632d2d85423119a1
EC2 Default User <ec2-user@ip-172-31-62-253.ec2.internal>
2021-03-08 09:46:37 +0000

    修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,26 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
   end
 
+  def create
+    @book = Book.new(book_params)
+    blog.save
+    redirect_to books_path
+  end
+
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
   def top
+    @book=Book.all
   end
 end
